f0026031ea064efb7f5b1f4592506d3167a4dbb2
unknown <pd14184@CEN4026594.ebre.pl>
2015-06-16 11:35:19 +0200

    Initial commit of the project.

---
+++ src/BruhUp/Informatyka.java
@@ -0,0 +1,63 @@
+package BruhUp;
+
+public class Informatyka {
+	
+	public final static char inf[] = {'I','n', 'f', 'o', 'r',
+		'm','a','t','y','k','a'};
+	
+	public static void print_v(){
+		
+		System.out.println("print_v:");
+		for (char c : inf) {
+			
+			System.out.println(c);
+		}
+		
+	}
+	
+	public static void print_h() {
+		
+		System.out.println("print_h:");
+		for (char c : inf) {
+			
+			System.out.print(c);
+		}
+		
+		System.out.print('\n');
+	}
+	
+	public static void print_V() {
+		
+		System.out.println("print_V:");
+		for (char c : inf) {
+			
+			c = Character.toUpperCase(c);
+			System.out.println(c);
+		}
+		
+		
+	}
+	
+	public static void print_H() {
+		
+		System.out.println("print_h:");
+		for (char c : inf) {
+			
+			c = Character.toUpperCase(c);
+			System.out.print(c);
+		}
+		
+		System.out.print('\n');
+	}
+	
+	public static void main(String[] args) {
+		
+		print_v();
+		print_V();
+		
+		print_h();
+		print_H();
+
+	}
+
+}
